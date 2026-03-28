655f1b580d3706bab59af409a09770ceb590f901
maxi <arora.anubhav93@gmail.com>
2026-03-28 13:15:00 +0530

    Update application id and namespace
    There was a typo in the namespace and app id

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -7,11 +7,11 @@ plugins {
 }
 
 android {
-    namespace = "com.maxi.unitconveter"
+    namespace = "com.maxi.unitconverter"
     compileSdk = 36
 
     defaultConfig {
-        applicationId = "com.maxi.unitconveter"
+        applicationId = "com.maxi.unitconverter"
         minSdk = 26
         targetSdk = 36
         versionCode = 1
