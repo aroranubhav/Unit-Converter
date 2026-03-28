d8cc5876d5efefdb55157e36b2ec8d8474472f42
maxi <arora.anubhav93@gmail.com>
2026-03-28 12:46:21 +0530

    Update MainActivity.kt to remove default composables

--- app/src/main/java/com/maxi/unitconveter/MainActivity.kt
+++ app/src/main/java/com/maxi/unitconveter/MainActivity.kt
@@ -4,44 +4,24 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.maxi.unitconveter.ui.theme.UnitConveterTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             UnitConveterTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                App()
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+fun App() {
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    UnitConveterTheme {
-        Greeting("Android")
-    }
 }
