1668bcf2c90f22fd874d0a9dd26fc65e90a8c73d
maxi <arora.anubhav93@gmail.com>
2026-03-28 16:44:51 +0530

    Add viewmodel dependency for compose

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -53,6 +53,8 @@ dependencies {
 
     // ViewModel
     implementation(libs.androidx.lifecycle.viewmodel)
+    implementation(libs.androidx.lifecycle.viewmodel.runtime)
+    implementation(libs.androidx.lifecycle.viewmodel.compose)
 
     // Hilt
     implementation(libs.hilt.android)
