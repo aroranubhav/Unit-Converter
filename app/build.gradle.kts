f54a0ad97100b0c0024bbbbec65d42796b2c7f05
maxi <arora.anubhav93@gmail.com>
2026-03-28 23:50:35 +0530

    Fix dark mode theme

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -66,6 +66,7 @@ dependencies {
     implementation(libs.androidx.compose.ui.graphics)
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
+    implementation(libs.material)
 
     // ViewModel
     implementation(libs.androidx.lifecycle.viewmodel)
